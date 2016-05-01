af88d2ac5665c98109f6e2d27953cb61ed9e15c0
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-30 22:21:45 -0500

    Create structure of main game loop

--- guessing_game/guess.rb
+++ guessing_game/guess.rb
@@ -10,3 +10,35 @@
 # what's about to happen
 
 puts "Welcome to the Guessing Game"
+puts "We will play #{Game::TOTAL_ROUNDS} rounds. Try to guess the number"
+puts "i'm thinking of in the fewest guesses."
+puts "You'll also estimate how many guesses it will take."
+puts "If you do it in fewer guesses, you'll gain bonus points!"
+
+#=======================================================
+
+# Set game variables
+
+print "What is your name? "
+name = gets.chomp
+player = Player.new(name)
+game = Game.new(player)
+
+#=======================================================
+
+# Create main game loop
+
+while !game.done? do
+  puts " "
+  puts "------> Round ##{game.round}"
+  puts " "
+  if game.get_high_number
+    if game.get_guess_count
+      game.prepare_computer_number
+      while !game.round_done? do
+        puts " "
+        game.get_player_guess
+        game.show_results
+      end
+      game.next_round
+end
