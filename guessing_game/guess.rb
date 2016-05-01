f2ad2b4d89d27a9d280d71d88a4fe4d7900d0850
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-30 22:03:04 -0500

    Create guess.rb, pseudocode game

---
+++ guessing_game/guess.rb
@@ -0,0 +1,12 @@
+#
+# Ruby For Kids Project 6: Guessing Game
+# Tutorial By: Chris Haupt
+# Programmed By: Kyrene Jackson
+# A guessing game in which the hints might not be true!
+#
+#=======================================================
+
+# Create a helpful message that will explain to the player
+# what's about to happen
+
+puts "Welcome to the Guessing Game"
