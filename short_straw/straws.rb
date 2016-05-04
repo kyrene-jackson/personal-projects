0aeb0634dc60c13b675813b64c15c402161ddb58
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-03 20:33:02 -0500

    Add game welcome message, create players constant with array of names

--- short_straw/straws.rb
+++ short_straw/straws.rb
@@ -16,3 +16,14 @@
 # The game will remove the player holding the shortest straw.
 # If there is more than one player remaining, a new round will start.
 # When only one player remains, the game will end and display the winner.
+
+#==========================================================================
+
+puts "Welcome to the Last Straw Game!"
+puts "In each round, players will draw straws of
+  two different lengths."
+puts "The players who pick the short straw will be
+  eliminated and"
+puts "a new round will begin, good luck!"
+
+PLAYERS = %w(Mitch Byron Andrey Dan Larry Cynthia Luna Amelia Peter Anthony)
