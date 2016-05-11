9891f9639d5ead9db265eddf4bc58c192d9107d9
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-10 20:19:38 -0500

    create new instance of codebreaker class, add welcome and ending message

--- code_breaker/codebreaker.rb
+++ code_breaker/codebreaker.rb
@@ -2,9 +2,8 @@
 # Ruby For Kids Project 8: Code Breaker
 # Tutorial By: Chris Haupt
 # Programmed By: Kyrene Jackson
-# A program that will encrypt and decrypt your notes
-# using the popular Ceaser Cipher!
-#
+# A program that will encrypt and decrypt another
+# document using the Ceaser cipher.
 # Program Skeleton
 # - A main object will manage the user interface
 # and input and output of files to be encrypted
@@ -14,5 +13,18 @@
 # - This programs version of the Ceaser Cipher
 # will use a lookup table technique organized
 # in a hash.
-
 #======================================================
+
+# welcome message to display to user once program is opened
+puts "Code Brekaer will encrypt or decrypt a file of your choice"
+puts " "
+
+# new CodeBreaker class instance
+codebreaker = CodeBreaker.new
+
+# status of whether or not the program worked properly
+if codebreaker.run
+  puts "All done!"
+else
+  puts "Didn't work!"
+end
