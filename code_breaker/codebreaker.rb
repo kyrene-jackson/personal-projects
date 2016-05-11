f80089596032a2f485b9d4737e3eda6fe885b88c
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-10 20:07:10 -0500

    Add program skeleton

---
+++ code_breaker/codebreaker.rb
@@ -0,0 +1,18 @@
+#
+# Ruby For Kids Project 8: Code Breaker
+# Tutorial By: Chris Haupt
+# Programmed By: Kyrene Jackson
+# A program that will encrypt and decrypt your notes
+# using the popular Ceaser Cipher!
+#
+# Program Skeleton
+# - A main object will manage the user interface
+# and input and output of files to be encrypted
+# and decrypted.
+# - A second object will contain the code for
+# the encryption process itself.
+# - This programs version of the Ceaser Cipher
+# will use a lookup table technique organized
+# in a hash.
+
+#======================================================
