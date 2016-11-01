3967269b72f52602a8dd9f0bc3f918007e623e67
Kyrene Jackson <ky.jackson01@gmail.com>
2016-11-01 18:47:04 -0500

    Add game methods

--- a-maze-ing/game.rb
+++ a-maze-ing/game.rb
@@ -35,12 +35,33 @@ class Game
   end
 
   def button_down(id)
+    @level.button_down(id)
   end
 
   def update
+    @level.update
+    if !@level.level_over?
+      @time_now = Time.now.to_i
+    end
   end
 
   def draw
+    @level.draw
+    draw_hud
+  end
+
+  def draw_hud
+    if @level.level_over?
+      @font.draw("GAME OVER!", 170, 150, 10, 2, 2)
+      @font.draw("Collected #{@player.score} treasure in #{time_in_seconds} seconds!", 110, 300, 10)
+    else
+      @font.draw("Time: #{time_in_seconds}", 4, 2, 10)
+      @font.draw("Score: #{@player.score}", 510, 2, 10)
+    end
+  end
+
+  def time_in_seconds
+    @time_now - @time_start
   end
 
 end
