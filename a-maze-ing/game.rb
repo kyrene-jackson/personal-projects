7f5b06cde95c8c86a30a0d40a52c1622d6a621a7
Kyrene Jackson <ky.jackson01@gmail.com>
2016-11-01 16:57:47 -0500

    Create game class, add initialize function

---
+++ a-maze-ing/game.rb
@@ -0,0 +1,25 @@
+require 'gosu'
+require_relative 'level'
+require_relative 'player'
+
+class Game
+  LEVEL1 = []
+
+  def initialize(window)
+    @window = window
+    @player = Player.new(@window, 0, 0)
+    @level = Level.new(@window, @player, LEVEL1)
+    @font = Gosu::Font.new(32)
+    @time_start = Time.now.to_i
+  end
+
+  def button_down(id)
+  end
+
+  def update
+  end
+
+  def draw
+  end
+
+end
