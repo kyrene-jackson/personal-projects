d98643bed56e7ee571f9beb2633f4ce8800c4a87
Kyrene Jackson <ky.jackson01@gmail.com>
2016-11-01 18:15:02 -0500

    Add remaining methods to amazing.rb

--- a-maze-ing/amazing.rb
+++ a-maze-ing/amazing.rb
@@ -11,14 +11,23 @@
 #=======================================================
 
 require 'gosu'
+require_relative 'game'
 
 class Amazing < Gosu::Window
   def initialize
     super(640, 640)
     self.caption = "Amazing"
-    # more code will go here
+    @game = Game.new(self)
   end
-  # even more code will go here
+  def update
+    @game.update
+  end
+  def draw
+    @game.draw
+  end
+  def button_down(id)
+    @game.button_down(id)
+  end
 end
 window = Amazing.new
 window.show
